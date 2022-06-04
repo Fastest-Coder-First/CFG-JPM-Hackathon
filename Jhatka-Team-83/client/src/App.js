4f1a57ec2638e7bd6a9659263909724e5cdecc40
Gauri Pethkar <gauri.pethkar7@gmail.com>
2022-06-04 13:36:48 +0530

    Navbar working

--- Jhatka-Team-83/client/src/App.js
+++ Jhatka-Team-83/client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Route, Routes, NavLink} from "react-router-dom";
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./components/Home";
@@ -8,25 +8,7 @@ import Home from "./components/Home";
 const App = () => {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-        <NavLink to="">React-Bootstrap</NavLink>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link to="#features">Features</Nav.Link>
-            <Nav.Link to="#pricing">Pricing</Nav.Link>
-            
-          </Nav>
-          <Nav>
-            <Nav.Link to="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} to="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <Navbar/>
         <Routes>
             <Route path= "/" element= {<Home/>}></Route>     
             <Route path = "/login" element = {<Login/>}></Route>
--- Jhatka-Team-83/client/src/components/Navbar.js
+++ Jhatka-Team-83/client/src/components/Navbar.js
@@ -7,14 +7,14 @@ function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light px-md-5">
-        <NavLink className="navbar-brand" to="/">MERN
+        <NavLink className="navbar-brand" to="/">Jhatka
         </NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item active">
               <NavLink className="nav-link" to="/">Home </NavLink>
             </li>
